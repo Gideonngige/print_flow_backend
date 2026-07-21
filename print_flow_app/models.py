cd2c0db0c44ddbdc2aff67bd1f1ce63af41376e8
Gideonngige <ushindigideon01@gmail.com>
2026-07-21 17:04:15 +0300

    modified apis

--- print_flow_app/models.py
+++ print_flow_app/models.py
@@ -42,7 +42,7 @@ class User(AbstractUser):
     username = models.CharField(max_length=150, unique=True) 
     full_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=20, unique=True)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="rider")
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="user")
     phone_verified = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
     email_verification_token = models.CharField(max_length=255, null=True, blank=True)
--- print_flow_app/urls.py
+++ print_flow_app/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path
 from . import views
+from .api_views.auth import *
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    # auth
+    path('send_test_email/', send_test_email, name='send_test_email'),
+    path('signup/', signup, name='signup'),
+    path('verify_email/', verify_email, name='verify_email'),
+    path('signin/', signin, name='signin'),
+    path('request_reset/', request_reset, name='request_reset'),
+    path('reset_password/', reset_password, name='reset_password'),
+    path('refresh_token', refresh_token, name='refresh_token'),
+    path('delete_account/', delete_account, name='delete_account'),
+    path('auth_check/', auth_check, name='auth_check'),
+    
 ]
